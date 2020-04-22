3b69a524db2a39ec419e8fdf4573ecc699423919
kenny181920 <38058919+kenny181920@users.noreply.github.com>
2020-04-21 23:56:00 -0500

    Login
    Se implemento el login y se hizo una reestructuracion de las carpetas.

---
+++ Generando API/API_python.py
@@ -0,0 +1,80 @@
+#!C:/Python3.8/python.exe
+
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api, reqparse
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+ambitos = {}
+
+ambitos.setdefault("cocina",["manzana","cuchara","platico","cebolla"])
+ambitos.setdefault("limpieza",["escoba","trapeador","jabon", "cuchara"])
+ambitos.setdefault("taller",["martillo","serrucho","desarmador"])
+
+parser = reqparse.RequestParser()
+
+@app.route('/productos', methods=['GET'])
+def listaGeneral():
+	return jsonify(ambitos)
+
+@app.route('/productos', methods=['POST'])
+def agregarProducto():
+	tipo = request.args.get('tipo')
+	ambitos.setdefault(tipo,[])
+	return jsonify(201)
+
+@app.route('/elementos', methods=['POST'])
+def agregarElementos():
+	tipo = request.args.get('tipo')
+	elemento = request.args.get('elemento')
+	if tipo == "cocina":
+		ambitos.setdefault("cocina", []).append(elemento)
+		return jsonify(201)
+	elif tipo == "limpieza":
+		ambitos.setdefault("limpieza", []).append(elemento)
+		return jsonify(201)
+	elif tipo == "taller":
+		ambitos.setdefault("taller", []).append(elemento)
+		return jsonify(201)
+	else:
+		return jsonify('No se encuentra el ambito')
+
+@app.route('/elementos', methods=['PUT'])
+def actualizarElemento():
+	tipo = request.args.get('tipo')
+	elemento = request.args.get('elemento')
+	ACelemento = request.args.get('ACelemento')
+	if (tipo in ambitos) and (elemento in ambitos[tipo]):
+		for x in range(len(ambitos[tipo])):
+			if ambitos[tipo][x] == elemento:
+				ambitos[tipo][x] = ACelemento
+		return jsonify('Actualizado')
+	else:
+		return jsonify('No encontrado')
+
+@app.route('/elementos', methods=['DELETE'])
+def eliminarElemento():
+	tipo = request.args.get('tipo')
+	elemento = request.args.get('elemento')
+	if (tipo in ambitos) and (elemento in ambitos[tipo]):
+		ambitos[tipo].remove(elemento)
+		return jsonify('Eliminado')
+	else:
+		return jsonify('No encontrado')
+
+@app.route('/elementos/busqueda', methods=['GET'])
+def busquedaElementos():
+	elemento = request.args.get('elemento')
+	resultado = []
+	for x in ambitos:
+		for y in ambitos[x]:
+			if y == elemento:
+				resultado.append(x)
+	return jsonify(resultado)
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ Login/login.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_httpauth import HTTPBasicAuth
+
+app = Flask(__name__)
+api = Api(app, prefix="/login")
+auth = HTTPBasicAuth()
+
+Data_Usuario = {
+	"kenny": "123456"
+}
+
+@auth.verify_password
+def verify(username, password):
+	if not(username and password):
+		return False
+	return Data_Usuario.get(username) == password
+
+class PrivateResource(Resource):
+	@auth.login_required
+	def get(self):
+		return "Login Exitoso"
+
+api.add_resource(PrivateResource, '/private')
+
+#@app.route('/')
+#@auth.login_required
+#def login():
+#	return "Login Exitoso" % auth.username()
+
+if __name__ == '__main__':
+	app.run(debug=True)
